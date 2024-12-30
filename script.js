f13ebf0e5dd3ed0198815362c728f1de288151e5
fcanale-git <checolesco@gmail.com>
2024-12-29 21:40:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Cambiar dinámicamente la clase "active" en los enlaces
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav ul li a');
+    links.forEach(link => {
+        if (link.href === window.location.href) {
+            link.classList.add('active');
+        }
+    });
+});
